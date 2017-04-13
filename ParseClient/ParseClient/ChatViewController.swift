b06b9993e5d76c167c5f38661e1711d94bd64269
Kevin Thrailkill <kevinjt92@gmail.com>
2017-04-12 20:50:17 -0700

    Milestone 6

--- ParseClient/ParseClient/ChatViewController.swift
+++ ParseClient/ParseClient/ChatViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     @IBOutlet weak var messageTextField: UITextField!
     var user: PFUser?
+    var image: UIImage?
     
     
     var chatArray : [PFObject] = []
@@ -64,19 +66,64 @@ class ChatViewController: UIViewController {
 
     @IBAction func composeClicked(_ sender: AnyObject) {
         
+        
         let msg = PFObject(className:"AkshayKevinMessage")
         msg["user"] = user!.username!
         msg["text"] = messageTextField.text!
-
+        if let image = image {
+            msg["photo"] = PFFile(data: UIImagePNGRepresentation(image)!)
+        }
 
         msg.saveInBackground { (success, error) in
             if success {
                 print(msg)
                 self.messageTextField.text = ""
+                
+                
             } else {
                 print(error!.localizedDescription)
             }
         }
+        image = nil
+    }
+    
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = false
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+
+        self.present(vc, animated: true, completion: nil)
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+    
+        let size = CGSize(width: 100.0, height: 100.0)
+        
+        
+        // Scale image to fit within specified size while maintaining aspect ratio
+        let aspectScaledToFitImage = originalImage.af_imageAspectScaled(toFit: size)
+        
+       image = aspectScaledToFitImage
+        
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
     }
     
     
