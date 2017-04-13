db3335c9979715deedd8ab090d62cbf98eb2ca00
Kevin Thrailkill <kevinjt92@gmail.com>
2017-04-12 19:37:43 -0700

    Milestone 2

---
+++ ParseClient/ParseClient/ChatViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ChatViewController.swift
+//  ParseClient
+//
+//  Created by Kevin Thrailkill on 4/12/17.
+//  Copyright © 2017 AkshayBhandary. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ChatViewController: UIViewController {
+
+    var user: PFUser?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(user)
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ParseClient/ParseClient/ViewController.swift
+++ ParseClient/ParseClient/ViewController.swift
@@ -7,9 +7,54 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    var passedUser: PFUser?
+    
+    
+    @IBAction func onLogin(_ sender: Any) {
+        
+        
+        
+        PFUser.logInWithUsername(inBackground: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if user != nil {
+                self.passedUser = user
+                self.performSegue(withIdentifier: "successSegue", sender: self)
+            }else{
+                print(error!.localizedDescription)
+            }
+        }
+        
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = emailTextField.text
+        user.password = passwordTextField.text
+        user.email = emailTextField.text
+    
+        user.signUpInBackground { (succeeded: Bool, error: Error?) in
+            if let error = error {
+                let errorString = error.localizedDescription
+                // Show the errorString somewhere and let the user try again.
+                print(errorString)
+                
+            } else {
+                self.passedUser = user
+                self.performSegue(withIdentifier: "successSegue", sender: self)
+                // Hooray! Let them use the app now.
+            }
+        }
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +63,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "successSegue" {
+            let chatVC = segue.destination as! ChatViewController
+            chatVC.user = passedUser
+            
+        }
     }
 
 
